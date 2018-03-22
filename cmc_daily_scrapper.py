956a5c03201f9f686e5b1fe0464c86a4742c2b59
mithunmanohar <mithunmanohar79@gmail.com>
2018-03-22 07:45:21 +0530

    cmc daily scrapper

--- cmc_daily_scrapper.py
+++ cmc_daily_scrapper.py
@@ -9,7 +9,7 @@ from bs4 import BeautifulSoup
 
 
 def connect_to_db():
-    db = Database()                                                             # connect to your googl cloud-sql database
+    db = Database()                                                             # connect to your google cloud-sql database
     return db
 
 def get_cmc_data():
@@ -22,9 +22,9 @@ def get_cmc_data():
     data = json.loads(k)
     return data
 
-def update_db(db, data_dict):
+def update_db(db, coin_data):
     query_str = """SELECT std_name from currencies where std_name = %s"""
-    		% data_dict['Name']
+    		% coin_data['Name']
     data = db.query(query_str)
     for each in data:
     	print each
@@ -39,39 +39,40 @@ def process_data(db, data):
     now = datetime.datetime.now()
     date_today = now.strftime("%Y-%m-%d")
     for rec in data:
-        coin = rec['Name']
-        res = db.cryptodata.find({"name" : coin})
-        if res.count() > 0:
-            up_data = {}
-            up_data['date'] = date_today
-            up_data['rank'] = rec['#']
-            up_data['ticker'] = rec['Symbol']
-            up_data['price'] = rec['Price']
-            up_data['% 7d'] = rec['% 7d']
-            up_data['Volume (24h)'] = rec['Volume (24h)']
-            up_data['% 24h'] = rec['% 24h']
-            up_data['market_cap'] = rec['Market Cap']
-            up_data['% 1hr'] = rec['% 1h']
-            up_data['ciculating_supply'] = rec['Circulating Supply']
-            db.cryptodata.update({"name" : coin},{'$push':{'data': up_data}})
-        else:
-            record = {}
-            db.cryptodata
-            record['name'] = coin
-            record['data'] = []
-            details = {}
-            details['date'] = date_today
-            details['rank'] = rec['#']
-            details['ticker'] = rec['Symbol']
-            details['price'] = rec['Price']
-            details['% 7d'] = rec['% 7d']
-            details['Volume (24h)'] = rec['Volume (24h)']
-            details['% 24h'] = rec['% 24h']
-            details['market_cap'] = rec['Market Cap']
-            details['% 1h'] = rec['% 1h']
-            details['circulating_supply'] = rec['Circulating Supply']
-            record['data'].append(details)
-            db.cryptodata.insert(record)
+	update_db(db, rec)
+	
+        #res = db.cryptodata.find({"name" : coin})
+        #if res.count() > 0:
+            #up_data = {}
+            #up_data['date'] = date_today
+            #up_data['rank'] = rec['#']
+            #up_data['ticker'] = rec['Symbol']
+            #up_data['price'] = rec['Price']
+            #up_data['% 7d'] = rec['% 7d']
+            #up_data['Volume (24h)'] = rec['Volume (24h)']
+            #up_data['% 24h'] = rec['% 24h']
+            #up_data['market_cap'] = rec['Market Cap']
+            #up_data['% 1hr'] = rec['% 1h']
+            #up_data['ciculating_supply'] = rec['Circulating Supply']
+            #db.cryptodata.update({"name" : coin},{'$push':{'data': up_data}})
+        #else:
+            #record = {}
+            #db.cryptodata
+            #record['name'] = coin
+            #record['data'] = []
+            #details = {}
+            #details['date'] = date_today
+            #details['rank'] = rec['#']
+            #details['ticker'] = rec['Symbol']
+            #details['price'] = rec['Price']
+            #details['% 7d'] = rec['% 7d']
+            #details['Volume (24h)'] = rec['Volume (24h)']
+            #details['% 24h'] = rec['% 24h']
+            #details['market_cap'] = rec['Market Cap']
+            #details['% 1h'] = rec['% 1h']
+            #details['circulating_supply'] = rec['Circulating Supply']
+            #record['data'].append(details)
+            #db.cryptodata.insert(record)
 
 
 if __name__ == '__main__':
