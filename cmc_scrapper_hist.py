c3ead3e7e3bfda7fb46449fef1eec7034ca526fd
mithunmanohar <mithunmanohar79@gmail.com>
2018-05-13 00:42:42 +0530

    new changes

--- cmc_scrapper_hist.py
+++ cmc_scrapper_hist.py
@@ -45,7 +45,8 @@ def get_cmc_data(url):
 
 def insert_data(db, data, his_date):
     now = datetime.datetime.now()
-    his_date = datetime.datetime.strptime('"'+his_date+'"', "%Y%m%d").strftime("%d/%m/%Y")
+    his_date = datetime.datetime.strptime(his_date, "%Y%m%d").strftime("%d/%m/%Y")
+    his_date = "'" + his_date + "'"
     for rec in data:
         rec = validate_rec(rec)
         #if rec["Symbol"] != "BTC":
