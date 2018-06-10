11f92ecc3eda57428d2fe4259aac1c4d8fe99010
mithunmanohar <mithunmanohar79@gmail.com>
2018-06-10 22:13:40 +0530

    changes

--- application.py
+++ application.py
@@ -52,8 +52,8 @@ def get_all_data():
     ret_data = []
 
     for each in data.fetchall():
-        #day_data = {str(each['date']) : str(each['rank'])}
-        day_data = str(each['date'])
+        day_data = {str(each['date']) : str(each['rank'])}
+        #day_data = str(each['date'])
         ret_data.append(day_data)
     return json.dumps(ret_data)
 
